448115fc3181099abdc19d2d6de852d02158a065
MOHD SOHIL KHAN <sohilkhan80889@gmail.com>
2025-09-18 19:53:47 +0000

    remove upgrade to pro features

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -13,13 +13,6 @@ import {
 import { usePathname } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import {
   SidebarProvider,
   Sidebar,
   SidebarHeader,
@@ -74,21 +67,6 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
                         ))}
                     </SidebarMenu>
                 </SidebarContent>
-                <SidebarFooter className="hidden md:flex">
-                     <Card>
-                        <CardHeader className="p-2 pt-0 md:p-4">
-                            <CardTitle>Upgrade to Pro</CardTitle>
-                            <CardDescription>
-                                Unlock all features and get unlimited access to our support team.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                            <Button size="sm" className="w-full">
-                                Upgrade
-                            </Button>
-                        </CardContent>
-                    </Card>
-                </SidebarFooter>
             </Sidebar>
             <SidebarInset className="flex flex-col">
                 <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6 sticky top-0 z-30">
